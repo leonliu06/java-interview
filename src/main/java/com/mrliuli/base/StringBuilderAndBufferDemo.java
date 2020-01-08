800b1b8ed78ff19a7d288be33fc6265ce2449386
leonliu06 <leonliu06@gmail.com>
2020-01-08 11:40:13 +0800

    StringBuilder

---
+++ src/main/java/com/mrliuli/base/StringBuilderAndBufferDemo.java
@@ -0,0 +1,15 @@
+package com.mrliuli.base;
+
+/**
+ * Created by liuli on 2020/01/08.
+ */
+public class StringBuilderAndBufferDemo {
+
+    /**
+     * StringBuilder 与 StringBuffer 区别：
+     * String：适用于少量的字符串操作。
+     * StringBuilder：since jdk1.5 适用于单线程下在字符串缓冲区进行大量操作。
+     * StringBuffer：since jdk1.0 适用于多线程下在字符串缓冲区进行大量操作。
+     */
+
+}
