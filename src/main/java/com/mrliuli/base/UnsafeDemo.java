a9c1d33ae93cf01d03dfd1ec7a0120c0bad37341
leonliu06 <leonliu06@gmail.com>
2020-01-10 11:14:41 +0800

    Unsafe.getObject

--- src/main/java/com/mrliuli/base/UnsafeDemo.java
+++ src/main/java/com/mrliuli/base/UnsafeDemo.java
@@ -34,12 +34,30 @@ public class UnsafeDemo {
      *      public native void unpark(Object var1); // unpark可以终止一个挂起的线程，使其恢复正常
      *    整个并发框架中对线程的挂起操作被封装在 LockSupport类中，LockSupport类中有各种版本park方法，但最终都调用了Unsafe.park()方法。
      *
+     * ConcurrentHashMap(jdk1.7)中用到的几个Unsafe类的方法：
+     * 1. getObject
+     *      // Fetches a reference value from a given Java variable.
+     *      public native Object getObject(Object o, long offset);
+     *   获取给定Java变量o中偏移地址为offset的一个引用值（属性的值）。此方法可突破修饰符的限制。类似的方法有getInt、getDouble等。
+     * 2. getObjectVolatile
+     *      // Fetches a reference value from a given Java variable, with volatile load semantics.
+     *      // Otherwise identical to {@link #getObject(Object, long)}
+     *      public native Object getObjectVolatile(Object o, long offset);
+     *   同上述getObject方法类似，但附加了'volatile'加载语义，即强制从主存中获取属性值。此方法要求要获取的属性被volatile修饰，
+     *   否则将和getObject方法相同。类似的方法还有getIntVolatile、getDoubleVolatile等。
+     *
+     * 3. compareAndSwapObject
+     *      // Atomically update Java variable to <tt>x</tt> if it is currently holding <tt>expected</tt>.
+     *      // @return <tt>true</tt> if successful
+     *      public final native boolean compareAndSwapObject(Object o, long offset, Object expected, Object x);
+     *   如果变量o中偏移地址为offset的属性值为expected的话，则更新该属性值为x，即CAS操作，该过程为原子性操作。更新成功返回true，否则false。
+     *   类似方法有compareAndSwapInt和compareAndSwapLong。
      *
      */
 
     public static void main(String[] args){
         try {
-            Unsafe unsafe = Unsafe.getUnsafe();
+            Unsafe unsafe = sun.misc.Unsafe.getUnsafe();
             System.out.println(unsafe.addressSize());
         } catch (SecurityException e){
             System.out.println(e.getMessage());
