0512019a9dc1f94eb643f0c3a5788d5252a8603d
leonliu06 <leonliu06@gmail.com>
2021-01-30 12:38:33 +0800

    抽象工厂

--- basics/src/main/java/com/mrliuli/design/factory/abstracd/Client.java
+++ basics/src/main/java/com/mrliuli/design/factory/abstracd/Client.java
@@ -4,6 +4,9 @@ package com.mrliuli.design.factory.abstracd;
  * @author liu.li
  * @date 2021/1/26
  * @description
+ *
+ * 抽象工厂模式是工厂方法模式的升级版本，工厂方法模式只生产一个等级的产品，而抽象工厂模式可生产多个等级的产品。
+ *
  */
 public class Client {
 
