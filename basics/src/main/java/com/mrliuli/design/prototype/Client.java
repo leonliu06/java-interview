f7601460d34f68a64a73d160eafb8c2cf548ef43
leonliu06 <leonliu06@gmail.com>
2021-02-04 18:08:02 +0800

    原型模式

--- basics/src/main/java/com/mrliuli/design/prototype/Client.java
+++ basics/src/main/java/com/mrliuli/design/prototype/Client.java
@@ -10,6 +10,8 @@ public class Client {
     public static void main(String[] args) throws CloneNotSupportedException {
 
         Realizetype obj1 = new Realizetype();
+
+        // 通过原型的一个实例对象来复制生成另一个实例对象
         Realizetype obj2 = (Realizetype) obj1.clone();
 
         System.out.println("obj1 == obj2 ? " + (obj1 == obj2));
--- basics/src/main/java/com/mrliuli/design/prototype/Realizetype.java
+++ basics/src/main/java/com/mrliuli/design/prototype/Realizetype.java
@@ -11,6 +11,11 @@ public class Realizetype implements Cloneable {
         System.out.println("具体原型创建成功！");
     }
 
+    /**
+     * 原型模式包含一个复制自己的方法
+     * @return
+     * @throws CloneNotSupportedException
+     */
     @Override
     protected Object clone() throws CloneNotSupportedException {
         System.out.println("具体原型复制成功！");
